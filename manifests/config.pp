8be3fbbe3a279c6095c3b73400905ed0e8cd7a5c
YoungKing <yanckin@gmail.com>
2014-09-03 13:32:08 +0800

    change config dir owner

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,10 +6,14 @@ class consul::config {
   $config_hash = $consul::config_hash
   file { $consul::config_dir:
     ensure => 'directory',
+    owner => $consul::user,
+    group => $consul::group,
   } ->
   file { 'config.json':
     path    => "${consul::config_dir}/config.json",
     content => template('consul/config.json.erb'),
+    owner => $consul::user,
+    group => $consul::group,
   }
 
 }
